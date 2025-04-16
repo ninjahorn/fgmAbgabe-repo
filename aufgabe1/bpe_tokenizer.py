56f47b52b51d63e26966d5a4c157aae13efa9a5b
gellertdLAPWSL <danielgellert09@gmail.com>
2025-04-16 16:20:01 +0200

    Added tokenizers for different datasets and vocab sizes

--- aufgabe1/bpe_tokenizer.py
+++ aufgabe1/bpe_tokenizer.py
@@ -22,11 +22,37 @@ def tokenize_text(tokenizer_path, text):
     
     encoding = tokenizer.encode(text)
     return encoding.tokens
+
+def tokenize_output(tokenizer_path, text):
+    tokens = tokenize_text(tokenizer_path, text)
+    print("--------------------------------------------------------------------------")
+    print("Tokenizer: ", tokenizer_path)
+    print("Anzahl Tokens:", len(tokens))
+    print("Tokens:", tokens)
+    
     
     
 if __name__ == "__main__":
+    # Trainieren der Tokenizer
     train_tokenizer("./datasets/bible_de.txt", 1000, "./trained_tokenizers/bible_de_bpe.json")
+    train_tokenizer("./datasets/bible_de.txt", 3000, "./trained_tokenizers/bible_de_bpe_3k.json")
     
-    tokens = tokenize_text("./trained_tokenizers/bible_de_bpe.json", "Das ist ein Test.")
-    print("Anzahl Tokens: ", len(tokens))
-    print("Tokens: ", tokens)
+    train_tokenizer("./datasets/bible_en.txt", 1000, "./trained_tokenizers/bible_en_bpe.json")
+    train_tokenizer("./datasets/bible_en.txt", 3000, "./trained_tokenizers/bible_en_bpe_3k.json")
+    
+    train_tokenizer("./datasets/bible_de-en.txt", 1000, "./trained_tokenizers/bible_de-en_bpe.json")
+    train_tokenizer("./datasets/bible_de-en.txt", 3000, "./trained_tokenizers/bible_de-en_bpe_3k.json")
+    
+    # Testen der Tokenizer
+    tokenize_output("./trained_tokenizers/bible_de_bpe.json", "Das ist ein Test.")
+    tokenize_output("./trained_tokenizers/bible_de_bpe_3k.json", "Das ist ein Test.")
+    
+    tokenize_output("./trained_tokenizers/bible_en_bpe.json", "This is a test.")
+    tokenize_output("./trained_tokenizers/bible_en_bpe_3k.json", "This is a test.")
+    
+    tokenize_output("./trained_tokenizers/bible_de-en_bpe.json", "Das ist ein Test.")
+    tokenize_output("./trained_tokenizers/bible_de-en_bpe_3k.json", "Das ist ein Test.")
+    tokenize_output("./trained_tokenizers/bible_de-en_bpe.json", "This is a test.")
+    tokenize_output("./trained_tokenizers/bible_de-en_bpe_3k.json", "This is a test.")
+    tokenize_output("./trained_tokenizers/bible_de-en_bpe.json", "This is ein Test.")
+    tokenize_output("./trained_tokenizers/bible_de-en_bpe_3k.json", "This is ein Test.")
